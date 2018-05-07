dfa3dedb9240b9abe7c0083aa11e5317f8b1e64b
AndreeaNe <38730266+AndreeaNe@users.noreply.github.com>
2018-05-07 10:54:00 +0300

    account login - first step

---
+++ HeCon/HeCon.Android/ListViewLogin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HeCon.Droid
+{
+    class ListViewLogin:BaseAdapter
+    {
+        Activity activity;
+        List<Account> lstAccounts;
+        LayoutInflater inflater;
+
+        public ListViewLogin(Activity activity, List<Account> lstAccounts)
+        {
+            this.activity = activity;
+            this.lstAccounts = lstAccounts;
+        }
+        public override int Count
+        {
+            get { return lstAccounts.Count; }
+        }
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            inflater = (LayoutInflater)activity.BaseContext.GetSystemService(Context.LayoutInflaterService);
+            View itemView = inflater.Inflate(Resource.Layout.login, null);
+            var email = itemView.FindViewById<TextView>(Resource.Id.emailEditText);
+            var password = itemView.FindViewById<TextView>(Resource.Id.passwordEditText);
+            if (lstAccounts.Count > 0)
+            {
+                email.Text = lstAccounts[position].Email;
+               password.Text = lstAccounts[position].Password;
+            }
+            return itemView;
+        }
+    }
+}
--- HeCon/HeCon.Android/MainActivity.cs
+++ HeCon/HeCon.Android/MainActivity.cs
@@ -6,21 +6,81 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+//using Firebase.js;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+//using Firebase.Xamarin;
 
 namespace HeCon.Droid
 {
     [Activity(Label = "HeCon", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+
+        private EditText input_email, input_password;
+        private ListView list_data;
+        private ProgressBar circular_progress;
+        private List<Account> list_users = new List<Account>();
+        private ListViewLogin adapter;
+        private Account selectedAccount;
+        private const string FirebaseURL = "hhttps://hecon-e36fa.firebaseio.com/"; //Firebase Database URL  
+
+
+         /*protected override void OnCreate(Bundle bundle)
+          {
+              TabLayoutResource = Resource.Layout.Tabbar;
+              ToolbarResource = Resource.Layout.Toolbar;
+
+              base.OnCreate(bundle);
+
+              global::Xamarin.Forms.Forms.Init(this, bundle);
+              LoadApplication(new App());
+          }*/
+
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource  
+          //  SetContentView(Resource.Layout.Main);
 
-            base.OnCreate(bundle);
-
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+         
+           // input_email = FindViewById<EditText>(Resource.Id.Email);
+           // input_password = FindViewById<EditText>(Resource.Id.Password);
+           // list_data = FindViewById<ListView>(Resource.Id.login);
+            list_data.ItemClick += (s, e) =>
+            {
+                Account account = list_users[e.Position];
+                selectedAccount = account;
+                input_email.Text = account.Email;
+                input_password.Text = account.Password;
+            };
+            await LoadData();
+        }
+        private async Task LoadData()
+        {
+          
+            list_data.Visibility = ViewStates.Invisible;
+            //var firebase = new FirebaseClient(FirebaseURL);
+           /* var items = await firebase
+                .Child("users")
+                .OnceAsync<Account>();
+            list_users.Clear();
+           // adapter = null;
+/*foreach (var item in items)
+            {
+                Account account = new Account();
+                account.Uid = item.Key;
+                account.Email = item.Object.Email;
+                account.Password = item.Object.Password;
+                list_users.Add(account);
+            }*/
+         //   adapter = new ListViewAdapter(this, list_users);
+           // adapter.NotifyDataSetChanged();
+          //  list_data.Adapter = adapter;
+            list_data.Visibility = ViewStates.Visible;
         }
     }
 }
