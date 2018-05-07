02913c48fc4d2b963dda0e697df53d0ac331ae27
AndreeaNe <38730266+AndreeaNe@users.noreply.github.com>
2018-05-07 19:48:51 +0300

    first step done - auth

--- HeCon/HeCon.Android/MainActivity.cs
+++ HeCon/HeCon.Android/MainActivity.cs
@@ -12,6 +12,8 @@ using System.Collections.Generic;
 using System;
 using System.Threading.Tasks;
 using Firebase.Auth;
+using Firebase;
+using Android.Text;
 
 namespace HeCon.Droid
 {
@@ -27,8 +29,9 @@ namespace HeCon.Droid
         EditText mPasswordField;
 
         // [START declare_auth]
-        //private FirebaseAuth mAuth;
-        
+        public static FirebaseApp app;
+        private FirebaseAuth mAuth;
+
         // [END declare_auth]
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -36,22 +39,129 @@ namespace HeCon.Droid
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.login);
 
+
             // Views
-          
+
             mEmailField = FindViewById<EditText>(Resource.Id.emailEditText);
             mPasswordField = FindViewById<EditText>(Resource.Id.passwordEditText);
 
             // Buttons
-            
-            FindViewById(Resource.Id.LogInButton).Click += async delegate {
-               // await CreateAccount(mEmailField.Text, mPasswordField.Text);
+
+            FindViewById(Resource.Id.LogInButton).Click += async delegate
+            {
+                await CreateAccount(mEmailField.Text, mPasswordField.Text);
             };
-            
+
 
             // [START initialize_auth]
-            //mAuth =Firebase.FirebaseOptions.
+            mAuth = FirebaseAuth.Instance;
             // [END initialize_auth]
         }
+        void AuthStateChanged(object sender, FirebaseAuth.AuthStateEventArgs e)
+        {
+            var user = e.Auth.CurrentUser;
+            if (user != null)
+            {
+                // User is signed in
+                Android.Util.Log.Debug(TAG, "onAuthStateChanged:signed_in:" + user.Uid);
+            }
+            else
+            {
+                // User is signed out
+                Android.Util.Log.Debug(TAG, "onAuthStateChanged:signed_out");
+            }
+            // [START_EXCLUDE]
+            //UpdateUI(user);
+            // [END_EXCLUDE]
+        }
+
+        // [START on_start_add_listener]
+        protected override void OnStart()
+        {
+            base.OnStart();
+            mAuth.AuthState += AuthStateChanged;
+        }
+        // [END on_start_add_listener]
+
+        // [START on_stop_remove_listener]
+        protected override void OnStop()
+        {
+            base.OnStop();
+            mAuth.AuthState -= AuthStateChanged;
+        }
+        // [END on_stop_remove_listener]
+
+        async Task CreateAccount(string email, string password)
+        {
+            Android.Util.Log.Debug(TAG, "createAccount:" + email);
+            if (!ValidateForm())
+                return;
+            // ShowProgressDialog();
+
+            try
+            {
+                await mAuth.CreateUserWithEmailAndPasswordAsync(email, password);
+            }
+            catch
+            {
+                Toast.MakeText(this, "Authentication failed.", ToastLength.Short).Show();
+            }
+
+            // [START_EXCLUDE]
+            //HideProgressDialog();
+            // [END_EXCLUDE]
+        }
+        bool ValidateForm()
+        {
+            var valid = true;
+
+            var email = mEmailField.Text;
+            if (TextUtils.IsEmpty(email))
+            {
+                mEmailField.Error = "Required.";
+                valid = false;
+            }
+            else
+            {
+                mEmailField.Error = null;
+            }
+
+            var password = mPasswordField.Text;
+            if (TextUtils.IsEmpty(password))
+            {
+                mPasswordField.Error = "Required.";
+                valid = false;
+            }
+            else
+            {
+                mPasswordField.Error = null;
+            }
+
+            return valid;
+        }
+
+        /* void UpdateUI(FirebaseUser user)
+         {
+             //HideProgressDialog();
+             if (user != null)
+             {
+                 mStatusTextView.Text = GetString(Resource.String.emailpassword_status_fmt, user.Email);
+                 mDetailTextView.Text = GetString(Resource.String.firebase_status_fmt, user.Uid);
+
+                // FindViewById(Resource.Id.email_password_buttons).Visibility = ViewStates.Gone;
+                // FindViewById(Resource.Id.email_password_fields).Visibility = ViewStates.Gone;
+                // FindViewById(Resource.Id.sign_out_button).Visibility = ViewStates.Visible;
+             }
+             else
+             {
+                 mStatusTextView.SetText(Resource.String.signed_out);
+                 mDetailTextView.Text = null;
+
+                 FindViewById(Resource.Id.email_password_buttons).Visibility = ViewStates.Visible;
+                 FindViewById(Resource.Id.email_password_fields).Visibility = ViewStates.Visible;
+                 FindViewById(Resource.Id.sign_out_button).Visibility = ViewStates.Gone;
+             }
+         }*/
 
 
 
@@ -69,4 +179,3 @@ namespace HeCon.Droid
 
     }
 }
-
--- HeCon/HeCon.Android/obj/Debug/android/src/md5d08ca25770d725f1019f1a6d7ad3dfdf/MainActivity.java
+++ HeCon/HeCon.Android/obj/Debug/android/src/md5d08ca25770d725f1019f1a6d7ad3dfdf/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity
 	static {
 		__md_methods = 
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onStart:()V:GetOnStartHandler\n" +
+			"n_onStop:()V:GetOnStopHandler\n" +
 			"";
 		mono.android.Runtime.register ("HeCon.Droid.MainActivity, HeCon.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
 	}
@@ -30,6 +32,22 @@ public class MainActivity
 	}
 
 	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onStart ()
+	{
+		n_onStart ();
+	}
+
+	private native void n_onStart ();
+
+
+	public void onStop ()
+	{
+		n_onStop ();
+	}
+
+	private native void n_onStop ();
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
